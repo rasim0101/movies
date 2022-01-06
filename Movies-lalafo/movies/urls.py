c390830066416a22588035336f192787c66c27ad
abatuF <nurbekuuludaniel@gmail.com>
2022-01-06 21:05:32 +0600

    new start

--- Movies-lalafo/movies/urls.py
+++ Movies-lalafo/movies/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from my_posts import views
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
